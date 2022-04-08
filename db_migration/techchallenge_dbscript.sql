c8411c44bb15a6090999f431892764d94990532e
jcinco <jcinco@parlaygames.com>
2022-04-09 07:33:45 +0800

    update db script

--- db_migration/techchallenge_dbscript.sql
+++ db_migration/techchallenge_dbscript.sql
@@ -2,6 +2,7 @@ CREATE TABLE `techchallenge`.`speech` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `actual_text` VARCHAR(100) NULL,
   `subject_text` VARCHAR(50) NULL,
+  `author` VARCHAR(50) NULL,
   `created_date` DATE NULL,
   `modified_date` DATE NULL,
   PRIMARY KEY (`id`));
